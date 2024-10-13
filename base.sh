69728e1d59b3b04a802706ebf67d5a7ffd5ef976
gp22 <gp22@users.noreply.github.com>
2024-10-13 17:24:33 -0600

    Update formatting

--- base.sh
+++ base.sh
@@ -6,101 +6,100 @@ echo
 
 PKGS=(
 
-    # SYSTEM --------------------------------------------------------------
+  # SYSTEM --------------------------------------------------------------
 
-    'networkmanager'
-    'nmtui'
-    'hyprland'
-    'waybar'
-    'dunst'
-    'hyprpaper'
-    'hypridle'
-    'xdg-desktop-portal-hyprland'
-    'xdg-user-dirs-gtk'
-    'wofi'
-    'noto-fonts'
-    'ttf-fira-sans'
-    'ttf-font-awesome'
-    'lxappearance'
-    'libadwaita'
-    'brightnessctl'
-    'kitty'
-    'alacritty'
-    'hyprlock'
-    'mousepad'
-    'file-roller'
-    'tumbler'
-    'ffmpegthumbnailer'
-    'gvfs-mtp galculator'
-    'ristretto'
-    'ttf-hack-nerd'
-    'ripgrep'
-    'thunar'
-    'thunar-archive-plugin'
-    'bluez'
-    'bluez-utils'
-    'blueman'
-    'wlsunset'
+  'networkmanager'
+  'nmtui'
+  'hyprland'
+  'waybar'
+  'dunst'
+  'hyprpaper'
+  'hypridle'
+  'xdg-desktop-portal-hyprland'
+  'xdg-user-dirs-gtk'
+  'wofi'
+  'noto-fonts'
+  'ttf-fira-sans'
+  'ttf-font-awesome'
+  'lxappearance'
+  'libadwaita'
+  'brightnessctl'
+  'kitty'
+  'alacritty'
+  'hyprlock'
+  'mousepad'
+  'file-roller'
+  'tumbler'
+  'ffmpegthumbnailer'
+  'gvfs-mtp galculator'
+  'ristretto'
+  'ttf-hack-nerd'
+  'ripgrep'
+  'thunar'
+  'thunar-archive-plugin'
+  'bluez'
+  'bluez-utils'
+  'blueman'
+  'wlsunset'
 
-    # TERMINAL UTILITIES --------------------------------------------------
+  # TERMINAL UTILITIES --------------------------------------------------
 
-    'curl'                  # Remote content retrieval
-    'man-db'                # Man pages
-    'man-pages'             # Man pages
-    'p7zip'                 # File archiver
-    'tmux'                  # Terminal multiplexer
-    'unzip'                 # Zip compression program
-    'wget'                  # Remote content retrieval
-    'zsh'                   # ZSH shell
+  'curl'      # Remote content retrieval
+  'man-db'    # Man pages
+  'man-pages' # Man pages
+  'p7zip'     # File archiver
+  'tmux'      # Terminal multiplexer
+  'unzip'     # Zip compression program
+  'wget'      # Remote content retrieval
+  'zsh'       # ZSH shell
 
-    # DISK UTILITIES ------------------------------------------------------
+  # DISK UTILITIES ------------------------------------------------------
 
-    # GENERAL UTILITIES ---------------------------------------------------
+  # GENERAL UTILITIES ---------------------------------------------------
 
-    # DEVELOPMENT ---------------------------------------------------------
+  # DEVELOPMENT ---------------------------------------------------------
 
-    'clang'                 # C Lang compiler
-    'cmake'                 # Cross-platform open-source make system
-    'git'                   # Version control system
-    'gcc'                   # C/C++ compiler
-    'gdb'                   # GNU Debugger
-    'glibc'                 # C libraries
-    'lazygit'               # Git terminal GUI
-    'neovim'                # Better than vim
-    'python'                # Scripting language
-    'vim'
+  'clang'   # C Lang compiler
+  'cmake'   # Cross-platform open-source make system
+  'git'     # Version control system
+  'gcc'     # C/C++ compiler
+  'gdb'     # GNU Debugger
+  'glibc'   # C libraries
+  'lazygit' # Git terminal GUI
+  'neovim'  # Better than vim
+  'python'  # Scripting language
+  'vim'
 
-    # WEB TOOLS -----------------------------------------------------------
+  # WEB TOOLS -----------------------------------------------------------
 
-    # COMMUNICATIONS ------------------------------------------------------
+  # COMMUNICATIONS ------------------------------------------------------
 
-    # MEDIA ---------------------------------------------------------------
+  # MEDIA ---------------------------------------------------------------
 
-    # GRAPHICS AND DESIGN -------------------------------------------------
+  # GRAPHICS AND DESIGN -------------------------------------------------
 
-    # PRODUCTIVITY --------------------------------------------------------
+  # PRODUCTIVITY --------------------------------------------------------
 
-    # VIRTUALIZATION ------------------------------------------------------
+  # VIRTUALIZATION ------------------------------------------------------
 
 )
 
 YAYPKGS=(
-    'wlogout'
-    'librewolf-bin'
-    'brave-bin'
-    'kmonad-bin'
+  'wlogout'
+  'librewolf-bin'
+  'brave-bin'
+  'kmonad-bin'
 )
 
 for PKG in "${PKGS[@]}"; do
-    echo "INSTALLING: ${PKG}"
-    sudo pacman -S "$PKG" --noconfirm --needed
+  echo "INSTALLING: ${PKG}"
+  sudo pacman -S "$PKG" --noconfirm --needed
 done
 
 for PKG in "${YAYPKGS[@]}"; do
-    echo "INSTALLING: ${PKG}"
-    yay -S "$PKG" --noconfirm
+  echo "INSTALLING: ${PKG}"
+  yay -S "$PKG" --noconfirm
 done
-
 
 echo
 echo "Done!"
